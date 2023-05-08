eac47c20dc20fa49c0c7f15371ae932cb9a8a0e5
MatheusKroska <kroskamatheus@gmail.com>
2023-05-08 20:50:09 -0300

    prod

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,62 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [step, setStep] = useState([])
+
+
+  function directMappingCache(memoryAddress, cacheSize) {
+  const cache = Array(cacheSize).fill(-1);
+  const cacheStep = [];
+  let hit = 0;
+  let miss = 0;
+
+  for (let i = 0; i < memoryAddress.length; i++) {
+    const cacheIndex = memoryAddress[i] % cacheSize;
+    if (cache[cacheIndex] === memoryAddress[i]) {
+      hit++;
+    } else {
+      cache[cacheIndex] = memoryAddress[i];
+      miss++;
+    }
+    cacheStep.push([...cache]);
+    }
+    
+    setStep(cacheStep);
+
+  return {
+    hit,
+    miss,
+    hitRate: hit / (hit + miss),
+    cacheStep,
+  };
+  }
+
+  useEffect(() => {
+    directMappingCache([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 4);    
+  }, [])
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {step &&
+        step.map((item, index) => {
+          return (
+            <div key={index}>
+              <p>Step {index}</p>
+              <p>Cache: {item.map((item, index) => {
+                return (
+                  <span key={index}>{item} </span>
+                )
+              })}</p>
+              <hr></hr>
+            </div>
+          )
+        })
+      }
     </>
   )
 }
